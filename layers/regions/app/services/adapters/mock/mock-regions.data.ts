7d41b4e4a8cd9061571ac53fa7bf84e64e9cf3f5
ravshan <ravshanselimov1@gmail.com>
2026-05-12 19:07:33 +0500

    replace hardcoded flag paths in mock regions with imported default icon

--- layers/regions/app/services/adapters/mock/mock-regions.data.ts
+++ layers/regions/app/services/adapters/mock/mock-regions.data.ts
@@ -1,35 +1,36 @@
-import {simpleFaker} from "@faker-js/faker";
-import type {IRegionDTO} from "#layers/regions/app/dto/regions.dto";
+import { simpleFaker } from "@faker-js/faker";
+import type { IRegionDTO } from "#layers/regions/app/dto/regions.dto";
+import icon from "./default-flag.svg";
 
 export const MOCK_REGIONS = [
 	{
 		id: simpleFaker.string.uuid(),
 		name: "Египет",
 		code: "EG" as const,
-		flag: "/default-flag.svg",
+		flag: icon,
 	},
 	{
 		id: simpleFaker.string.uuid(),
 		name: "Индия",
 		code: "IN" as const,
-		flag: "/default-flag.svg",
+		flag: icon,
 	},
 	{
 		id: simpleFaker.string.uuid(),
 		name: "Нигерия",
 		code: "NG" as const,
-		flag: "/default-flag.svg",
+		flag: icon,
 	},
 	{
 		id: simpleFaker.string.uuid(),
 		name: "Нидерланды",
 		code: "NL" as const,
-		flag: "/default-flag.svg",
+		flag: icon,
 	},
 	{
 		id: simpleFaker.string.uuid(),
 		name: "Бразилия",
 		code: "BR" as const,
-		flag: "/default-flag.svg",
+		flag: icon,
 	},
-] satisfies IRegionDTO[]
+] satisfies IRegionDTO[];
