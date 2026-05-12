d2c0ca4d49ffe814e3bcf97dd847b7ca881c844e
ravshan <ravshanselimov1@gmail.com>
2026-05-12 11:42:34 +0500

    RegionsGroup use css modules

--- layers/regions/app/components/RegionsGroup/RegionsGroup.vue
+++ layers/regions/app/components/RegionsGroup/RegionsGroup.vue
@@ -2,6 +2,8 @@
 import {RadioGroupItem, RadioGroupRoot} from 'radix-vue'
 import type {IRegionDTO} from "#layers/regions/app/dto/regions.dto";
 
+import styles from './RegionsGroup.module.css'
+
 export interface IRegionsGroupProps {
   regions: IRegionDTO[]
 }
@@ -13,54 +15,17 @@ const selectedRegionID = defineModel<string | undefined>("selectedRegionID", { r
 
 <template>
   <RadioGroupRoot
-    class="root"
+    :class="styles.root"
     v-model="selectedRegionID"
   >
     <RadioGroupItem
       v-for="region in regions" :key="region.id"
       :id="region.id"
       :value="region.id"
-      class="item"
+      :class="styles.item"
     >
       <img :src="region.flag ?? ''" alt="flag" />
       <span>{{ region.name }}</span>
     </RadioGroupItem>
   </RadioGroupRoot>
 </template>
-
-<style scoped>
-.root{
-  display: flex;
-  gap: var(--gap-sm)
-}
-
-.item {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 138px;
-  height: 44px;
-  gap: 4px;
-  padding: 13px 22px;
-
-  background-color: var(--surface-bg-color-100);
-  color: var(--text-base-color);
-  border-radius: 54px;
-  cursor: pointer;
-  transition: all 0.2s;
-
-  &:hover {
-    opacity: 0.7;
-  }
-}
-.item[data-state="checked"] {
-  background-color: #E8E8E8;
-  color: var(--text-alternate-color);
-}
-
-.item span {
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-</style>
