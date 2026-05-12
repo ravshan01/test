9f2f6d96d749904f84eadc74dd75e8400cfc972c
ravshan <ravshanselimov1@gmail.com>
2026-05-12 13:58:09 +0500

    remove subscriptionPlansByRegionsAndPlanTypes from useCatalogStore

--- layers/catalog/app/stores/catalog.store.ts
+++ layers/catalog/app/stores/catalog.store.ts
@@ -1,7 +1,4 @@
 import type { ICatalogDTO } from "~~/layers/catalog/app/dto/catalog.dto";
-import type { IPlanTypeDTO } from "~~/layers/plans/app/dto/plan-types.dto";
-import type { ISubscriptionPlanDTO } from "~~/layers/plans/app/dto/subscription-plans.dto";
-import type { IRegionDTO } from "~~/layers/regions/app/dto/regions.dto";
 
 export const useCatalogStore = defineStore("catalog", {
 	state: () => ({
@@ -9,25 +6,6 @@ export const useCatalogStore = defineStore("catalog", {
 		selectedRegionID: undefined as string | undefined,
 		selectedSubscriptionPlanIDs: [] as string[],
 	}),
-
-	getters: {
-		subscriptionPlansByRegionsAndPlanTypes: ({ data }) => {
-			const result: Record<IRegionDTO["id"], Record<IPlanTypeDTO["id"], ISubscriptionPlanDTO[]>> = {};
-
-			data?.regions.forEach((region) => {
-				result[region.id] = {};
-
-				data.planTypes.forEach((planType) => {
-					result[region.id]![planType.id] =
-						data.subscriptionPlans.filter(
-							(el) => el.regionIDs.includes(region.id) && el.typeID === planType.id,
-						) ?? [];
-				});
-			});
-
-			return result;
-		},
-	},
 
 	actions: {
 		toggleSelectSubscriptionPlan(id: string) {
