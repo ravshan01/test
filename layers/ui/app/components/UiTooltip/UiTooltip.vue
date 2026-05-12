84bd110f0eeb5e232b10bf8aa348a46450ef001f
ravshan <ravshanselimov1@gmail.com>
2026-05-12 14:05:54 +0500

    create UiTooltip

---
+++ layers/ui/app/components/UiTooltip/UiTooltip.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {TooltipContent, TooltipPortal, TooltipRoot, TooltipTrigger} from 'radix-vue'
+
+import styles from './UiTooltip.module.css'
+
+export interface IUiTooltipProps {
+  content: string
+}
+
+defineProps<IUiTooltipProps>()
+</script>
+
+<template>
+  <TooltipRoot>
+    <TooltipTrigger class="help-icon-trigger">
+      <span :class="styles.icon">?</span>
+    </TooltipTrigger>
+
+    <TooltipPortal>
+      <TooltipContent :class="styles.content">
+        {{ content }}
+      </TooltipContent>
+    </TooltipPortal>
+  </TooltipRoot>
+</template>
