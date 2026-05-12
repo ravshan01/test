49ae5fc8caa69e834844286b000afaf3f453bbb2
ravshan <ravshanselimov1@gmail.com>
2026-05-12 11:43:14 +0500

    update catalog view

--- layers/catalog/app/pages/catalog.vue
+++ layers/catalog/app/pages/catalog.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import {RegionsGroup} from "#components";
 import {useCatalogView} from "#layers/catalog/app/composables/use-catalog-view";
+import RegionsGroup from "#layers/regions/app/components/RegionsGroup/RegionsGroup.vue";
 
 const {data, selectedRegionID} = useCatalogView()
 </script>
@@ -8,21 +8,25 @@ const {data, selectedRegionID} = useCatalogView()
 
 <template>
   <div class="root">
-    <div class="content">
-      <template v-if="data">
+    <template v-if="data">
+      <div class="header">
         <RegionsGroup :regions="data.regions" v-model:selectedRegionID="selectedRegionID" />
-      </template>
-    </div>
+      </div>
+
+      <div class="content"></div>
+    </template>
   </div>
 </template>
 
 <style scoped>
 .root {
   display: flex;
-  justify-content: center;
-  padding: var(--padding-xl)
+  flex-direction: column;
+  align-items: center;
+  padding: var(--padding-xl);
+  gap: 28px;
 }
-.content {
+.header, .content {
   width: 100%;
   max-width: 722px;
 }
