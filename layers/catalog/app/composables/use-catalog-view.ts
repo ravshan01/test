8176e0bf633de226e59871cc4ba3ff669e22f7c5
ravshan <ravshanselimov1@gmail.com>
2026-05-12 11:24:18 +0500

    create and use useCatalogView

---
+++ layers/catalog/app/composables/use-catalog-view.ts
@@ -0,0 +1,24 @@
+import { useCatalogStore } from "#layers/catalog/app/stores/catalog.store";
+
+export function useCatalogView() {
+	const { data } = useCatalogData();
+	const store = useCatalogStore();
+
+	watchEffect(() => {
+		if (
+			data.value?.regions &&
+			data.value.regions.length > 0 &&
+			!store.selectedRegionID
+		)
+			store.selectedRegionID = data.value.regions[0]!.id;
+	});
+
+	const selectedRegionID = computed({
+		get: () => store.selectedRegionID ?? data.value?.regions?.[0]?.id,
+		set: (val) => {
+			store.selectedRegionID = val;
+		},
+	});
+
+	return { data, selectedRegionID };
+}
---
+++ layers/catalog/app/pages/catalog.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {RegionsGroup} from "#components";
+import {useCatalogView} from "#layers/catalog/app/composables/use-catalog-view";
+
+const {data, selectedRegionID} = useCatalogView()
+</script>
+
+
+<template>
+  <div class="root">
+    <div class="content">
+      <template v-if="data">
+        <RegionsGroup :regions="data.regions" v-model:selectedRegionID="selectedRegionID" />
+      </template>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.root {
+  display: flex;
+  justify-content: center;
+  padding: var(--padding-xl)
+}
+.content {
+  width: 100%;
+  max-width: 722px;
+}
+</style>
