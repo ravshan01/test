aea47e0c2ed9b5ad9c341270bd742624b74935d5
ravshan <ravshanselimov1@gmail.com>
2026-05-12 10:31:36 +0500

    create catalog.vue

---
+++ app/pages/catalog.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {useCatalogData} from "~~/layers/catalog/app/composables/use-catalog-data";
+
+const {data, pending, error}  = useCatalogData()
+</script>
+
+
+<template>
+  <div class="root">
+    <div class="content">
+      CATALOG PAGE
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.root {
+  display: flex;
+  justify-content: center;
+  padding: var(--padding-xl)
+}
+.content {
+  width: 100%;
+  max-width: 722px;
+}
+</style>
