510041aebe312cba1a1d723699d580c93494d9fa
ravshan <ravshanselimov1@gmail.com>
2026-05-12 11:20:21 +0500

    update catalog view

--- app/pages/catalog.vue
+++ app/pages/catalog.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
+import {useCatalogStore} from "#layers/catalog/app/stores/catalog.store";
+import RegionsGroup from "#layers/regions/app/components/RegionsGroup/RegionsGroup.vue";
 import {useCatalogData} from "~~/layers/catalog/app/composables/use-catalog-data";
 
-const {data, pending, error}  = useCatalogData()
+const {data}  = useCatalogData()
+const store = useCatalogStore()
+
+watchEffect(() => {
+  if (data.value?.regions && data.value.regions.length > 0 && !store.selectedRegionID)
+    store.selectedRegionID = data.value.regions[0]!.id
+})
+
+const selectedRegionID = computed({
+  get: () => store.selectedRegionID ?? data.value?.regions?.[0]?.id,
+  set: (val) => { store.selectedRegionID = val },
+ })
 </script>
 
 
 <template>
   <div class="root">
     <div class="content">
-      CATALOG PAGE
+      <template v-if="data">
+        <RegionsGroup :regions="data.regions" v-model:selectedRegionID="selectedRegionID" />
+      </template>
     </div>
   </div>
 </template>
