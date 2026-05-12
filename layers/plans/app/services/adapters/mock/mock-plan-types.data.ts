3b906b96d4fc8991b0d6ca13356e255d82f1cb8f
ravshan <ravshanselimov1@gmail.com>
2026-05-12 13:55:00 +0500

    update MOCK_PLAN_TYPES

--- layers/plans/app/services/adapters/mock/mock-plan-types.data.ts
+++ layers/plans/app/services/adapters/mock/mock-plan-types.data.ts
@@ -1,4 +1,4 @@
-import { simpleFaker } from "@faker-js/faker";
+import { fakerRU, simpleFaker } from "@faker-js/faker";
 import type { IPlanTypeDTO } from "~~/layers/plans/app/dto/plan-types.dto";
 
 export const MOCK_PLAN_TYPES = [
@@ -6,28 +6,28 @@ export const MOCK_PLAN_TYPES = [
 		id: simpleFaker.string.uuid(),
 		code: "Individual" as const,
 		name: "Индивидуальные подписки",
-		description: "",
+		description: fakerRU.lorem.paragraph(),
 		accountsCount: 1,
 	},
 	{
 		id: simpleFaker.string.uuid(),
 		code: "DUO" as const,
 		name: "Парные подписки",
-		description: "",
+		description: fakerRU.lorem.paragraph(),
 		accountsCount: 2,
 	},
 	{
 		id: simpleFaker.string.uuid(),
 		code: "Family" as const,
 		name: "Семейные подписки",
-		description: "",
+		description: fakerRU.lorem.paragraph(),
 		accountsCount: 6,
 	},
 	{
 		id: simpleFaker.string.uuid(),
 		code: "Platinum" as const,
 		name: "Платиновая подписка",
-		description: "",
+		description: fakerRU.lorem.paragraph(),
 		accountsCount: 3,
 	},
 ] satisfies IPlanTypeDTO[];
